7fdf0f37e305c8286eed87f2ee45a2cd682daf94
FBW <fw@speedpoint.de>
2014-07-21 08:55:42 +0200

    In den Einstellungen am Scriptanfang kann man jetzt folgende Möglichkeiten bei der Dokumentnamen-Eingabe wählen: 1) Nur Abfrage des Dokumentnamens. Dokument wird direkt im Patordner gespeichert 2) Nach der Abfrage des Dokumentnamens wird der Name eines Unterordners abgefragt. Dieser Unterordner wird im Patordner angelegt u. dort das Scandokument gespeichert.     Dabei gibt es wieder 2 Möglichkeiten:      -  Im Ordnernamen-Abfragefeld steht ein Default (kann überschrieben werden)      -  Das Abfragefeld ist leer. Dann muss jedes Mal ein etwas eingetragen werden.
    Außerdem wird überprüft ob die Eingabefelder leer sind, ob der Unterordner angelegt werden konnte u. ob das Dokument im Patordner gespeichert wurde.

--- xscan.sh
+++ xscan.sh
@@ -1,64 +1,96 @@
 #!/bin/bash
 
+# Zum Scannen in einen pat_nr Pat.ordner. Basierend auf patordner.sh
+# Speedpoint GmbH (MR,FW), Stand: Juli 2014, Version 3
 
-# Zum Scannen in die DV Dokumentenablage (pat_nr). 
-# Speedpoint GmbH (MR,FW), Stand: Oktober 2013, Version 3 (Xdialog)
+# Bitte anpassen:    ###########################################################
 #
-# DisplayAusgabe.sh wird am Linux Server vorausgesetzt,
-# am Windows PC wird Rexserver benoetigt.
-
-
-# Bitte anpassen:  #############################################################
-#
-ip="192.168.0.10"                    # IP des WinPC (Rexserver!)
+ip="192.168.0.20"                    # IP des WinPC (Rexserver!)
 port="6666"                          # Port fuer Rexserver
 batch="c:\\david\startscan.bat"      # Batchdatei am WinPC, "c:\\" beachten!!
 #
-################################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Ab hier bitte Finger weg!
-
-
-# Fehler ausgeben, falls aktuelles X Display nicht gefunden wurde:
 #
-ichbins=`whoami`
-if [ ! -e /home/$ichbins/DisplayAusgabe ]; then
-   echo ""
-   echo "*****************************************************************"
-   echo "Scanmodul nicht initialisiert, wurde DisplayAusgabe.sh gestartet?"
-   echo "*****************************************************************"
-   echo ""
-   exit 1
-fi
-
-
-export XAUTHORITY=/home/$ichbins/.Xauthority
-export DISPLAY=`cat /home/$ichbins/DisplayAusgabe`
-
+# Unterordner-Abfrage, in dem die Scandateien gespeichert werden sollen:
+# -----------------------------------------------------------------------
+# - wenn Unterordner-Abfrage nicht erwünscht (Scan-Dokumente werden direkt im Patordner gespeichert):
+#   scanfilesubdir="" setzen (nichts)
+# - wenn Abfrage erwünscht:
+#   - Default-Vorschlag in der Abfrage (kann in Abfrage überschrieben werden): 
+#     scanfilesubdir=DEFAULTORDNER-NAMEN      z.B. scanfilesubdir="Scan"
+#   - leere Abfrage ohne Vorschlag:
+#     scanfilesubdir="LEER"                   alles groß schreiben!
+scanfilesubdir="LEER"
+#
+#
+################################################################################
 
 # Und los...
 
+function abfrage ()
+{
+# Übergabewerte
+typ=$1     # fnam oder dirnam
+dirdefault=$2
+[ "$typ" = "fnam" ] && dtext="Dokumentnamen" || dtext="Unterordner-Namen"
+eingabeloop=1
+while [ $eingabeloop = 1 ]; do
+   eingabeloop=0
+   if [ "$ostype" = "suse" ]; then
+      eingabe=`kdialog --inputbox "Bitte den $dtext eingeben:" "$dirdefault"`
+      retval=$?
+   else
+      /usr/bin/Xdialog --title "Scanmodul" --inputbox "Bitte den $dtext eingeben:" 0 0 "$dirdefault"  2>/tmp/scaninputbox.tmp.$$ 
+      retval=$?
+      eingabe=`cat /tmp/scaninputbox.tmp.$$`
+      rm -f /tmp/scaninputbox.tmp.$$
+   fi
+   if [ $retval = 1 ]; then
+      rm -f $link/*
+      if [ "$ostype" = "suse" ]; then
+         kdialog --error "ABBRUCH, Dokument wurde nicht gespeichert."
+      else
+         /usr/bin/Xdialog --title "Scanmodul" --msgbox "ABBRUCH, Dokument wurde nicht gespeichert." 0 0
+      fi
+      exit 1
+   fi
+
+   # Test auf leere Eingabe
+   eingabe=`echo $eingabe|sed 's/ //g'`
+   if [ "$eingabe" = "" ]; then
+      if [ "$ostype" = "suse" ]; then
+         kdialog --yesno "Falsche Eingabe: Kein $dtext eingegeben. Eingabe wiederholen?"
+         retval=$?
+      else
+         /usr/bin/Xdialog --title "Scanmodul" --yesno "Falsche Eingabe: Kein $stext eingegeben. Eingabe wiederholen?" 0 0
+         retval=$?
+      fi
+      if [ $retval = 1 ]; then
+         if [ "$ostype" = "suse" ]; then
+            kdialog --error "ABBRUCH, Dokument wurde nicht gespeichert."
+         else
+            /usr/bin/Xdialog --title "Scanmodul" --msgbox "ABBRUCH, Dokument wurde nicht gespeichert." 0 0
+         fi
+         exit 1
+      else
+         eingabeloop=1
+      fi
+   fi
+done
+}
+
+
+# welches Betriebssystem?
+[ "`cat /etc/issue|fgrep "Scientific"`" ]  && ostype="sl" || ostype="suse"
+
+# evtl. Leerzeichen aus Unterordnernamen entfernen
+scanfilesubdir=`echo $scanfilesubdir|sed 's/ /_/g'`
+# falls Unterordnernamen nur ein Leerzeichen enthält (falscher Eintrag)
+[ "$scanfilesubdir" = "_" ] && scanfilesubdir=""
 
 # Welcher Patient ist gerade augerufen?
 serverpfad=$DAV_HOME/trpword/pat_nr
 pfad=`echo $1 | awk '{printf("%08.f\n",$1)}' \
               | awk -F '' '{printf("%d/%d/%d/%d/%d/%d/%d/%d",$1,$2,$3,$4,$5,$6,$7,$8)}'`
-fullpfad=$serverpfad/$2/$pfad
-mkdir -p -m 0777 "$fullpfad" > "/dev/null" 2>&1 
- 
 
 # Leeres Zwischenziel fuer gescantes PDF anlegen:
 link="/home/david/trpword/Patient"
@@ -68,34 +100,62 @@ fi
 rm -f $link/* 2>>/dev/null
 
 if [ ! -d $link ]; then
-   Xdialog --title "Abbruch" --msgbox "Pat.ordner konnte nicht angelegt werden, bitte Speedpoint anrufen." 6 80
+   if [ "$ostype" = "suse" ]; then
+      kdialog --sorry "Pat.ordner konnte nicht angelegt werden, bitte Speedpoint anrufen."
+   else
+      /usr/bin/Xdialog --title "Scanmodul" --msgbox "Pat.ordner konnte nicht angelegt werden, bitte Speedpoint anrufen." 0 0
+   fi
    exit 1
 fi
-
 
 #Batchscan am Windowsrechner starten:
 echo "DAVCMD start /min $batch" | netcat $ip $port >/dev/null
 
-
 # Dokumentennamen erfragen:
-Xdialog --cancel-label "Abbruch"            \
-        --title "Dokumentennamen eingeben"  \
-        --clear                             \
-        --inputbox "- Bitte einen Dateinamen (ohne Endung) eingeben: " 20 80 Dokument 2>/tmp/inbox.tmp.$$
+abfrage "fnam"
+docnam=$eingabe
 
-if [ $? = 1 ]; then
-   rm -f $link/*
-   Xdialog --title "Abbruch durch Benutzer" --msgbox "Das Dokument wurde nicht gespeichert." 6 70
-   exit 1
+# Subdirectory  erfragen:
+if [ "$scanfilesubdir" = "" ]; then
+   # kein Abfragedialog, direkt im Patordner speichern
+   fullpfad=$serverpfad/$2/$pfad
+else
+   [ "$scanfilesubdir" = "LEER" ] && scanfilesubdir=""
+   abfrage "dirnam" $scanfilesubdir
+   subdirnam=$eingabe
+   # falls vorhanden das / am Anfang abschneiden
+   [ "`echo $subdirnam|cut -c1`" = "/" ] && subdirnam=`echo $subdirnam|cut -c2-`
+   # falls vorhanden das / am Ende abschneiden
+   [ "`echo $subdirnam|rev|cut -c1`" = "/" ] && subdirnam=`echo $subdirnam|sed 's/.$//'`
+   # Leerzeichen durch _ ersetzen
+   scanfilesubdir=`echo $subdirnam | sed 's/ /_/g'`
+   fullpfad=$serverpfad/$2/$pfad/$scanfilesubdir
+
+   # Subdirectory anlegen
+   mkdir -p -m 0777 "$fullpfad" > "/dev/null" 2>&1 
+
+   if [ ! -d $fullpfad ]; then
+      if [ "$ostype" = "suse" ]; then
+         kdialog --sorry "Unterordner >$scanfilesubdir< konnte nicht angelegt werden, bitte Speedpoint anrufen."
+      else
+         /usr/bin/Xdialog --title "Scanmodul" --msgbox "Unterordner >$scanfilesubdir< konnte nicht angelegt werden, bitte Speedpoint anrufen." 0 0
+      fi
+      exit 1
+   fi
 fi
 
-
-# PDF in Dokumentenablage speichern:
-docnam=`cat /tmp/inbox.tmp.$$`
+# PDF speichern:
 docpdf=`echo $docnam | sed 's/ /_/g'`
 cp -pf $link/*.pdf $fullpfad/$docpdf.pdf
 rm -f $link/*
-
-rm -f /tmp/inbox.tmp.$$
+# Überprüfen
+if [ ! -e $fullpfad/$docpdf.pdf ]; then
+   if [ "$ostype" = "suse" ]; then
+      kdialog --sorry "Das Dokument konnte nicht im Patordner gespeichert werden, bitte Speedpoint anrufen."
+   else
+      /usr/bin/Xdialog --title "Scanmodul" --msgbox "Das Dokument konnte nicht im Patordner gespeichert werden, bitte Speedpoint anrufen." 0 0
+   fi
+   exit 1
+fi
 
 exit 0
